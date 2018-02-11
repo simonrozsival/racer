ed30c94f20c5e3f13fd2c15351e27b5e8461358b
Simon Rozsival <simon@rozsival.com>
2018-02-11 16:32:22 +0000

    :car: Update the raspberry setup

--- racer-raspberry/src/initialize.bash
+++ racer-raspberry/src/initialize.bash
@@ -8,8 +8,14 @@ cd /ros_catkin_ws/src
 catkin_init_workspace
 
 # copy custom robot package into the workspace
-echo "> copy custom package into the workspace"
-cp -r /robot/src/racer_package .
+# echo "> copy custom package into the workspace"
+# cp -r /robot/src/racer_package .
+
+# setup the Scanse Sweep IMU
+git clone https://github.com/scanse/sweep-ros.git
+cd sweep-ros
+rosdep install -a -y -r
+cd ..
 
 # build the workspace
 echo "> build the workspace"
