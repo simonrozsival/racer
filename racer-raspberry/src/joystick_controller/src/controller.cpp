e417d0694135d2b8cad3ab1676f21453493ebc82
simonrozsival <simon@rozsival.com>
2018-03-24 15:03:27 +0100

    :construction: Update the joystick controller

--- racer-raspberry/src/joystick_controller/src/controller.cpp
+++ racer-raspberry/src/joystick_controller/src/controller.cpp
@@ -2,62 +2,11 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
+#include "JoystickTransformer.h"
+
 #define DRIVING_TOPIC "/racer/driving/commands"
 
-#define DRIVE_BUTTON 0
-#define THROTTLE_AXIS 1
-#define STEERING_AXIS 0
-
-class JoystickTransformer
-{
-  public:
-    JoystickTransformer(const ros::Publisher& publisher);
-    void process_joystick_input(const sensor_msgs::Joy::ConstPtr& msg);
-
-  private:
-    const ros::Publisher& steering_;
-
-    void move(float throttle, float angle);
-    void stop();
-}
-
-JoystickTransformer::JoystickTransformer(const ros::Publisher& publisher)
-  : steering_(publisher)
-{
-}
-
-void JoystickTransformer::process_joystick_input(const sensor_msgs::Joy::ConstPtr& msg)
-{
-  bool stop = !msg[DRIVE_BUTTON];
-  if (stop)
-  {
-    stop();
-    return;
-  }
-
-  float throttle = joy->axes[TROTTLE_AXIS];
-  float angle = joy->axes[STEERING_AXIS];
-
-  move(throttle, angle);
-}
-
-void JoystickTransformer::stop()
-{ 
-  move(0, 0);
-}
-
-void JoystickTransformer::move(float throttle, float angle)
-{
-  throttle = throttle > 0 ? throttle : 0;
-  
-  geometry_msgs::Twist msg;
-  msg.linear.x = throttle;
-  msg.angular.z = angle;
-  
-  steering_.publish(msg);
-}
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "joystick_controller");
   ros::NodeHandle nh;
