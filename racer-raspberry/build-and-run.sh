f26df6153d9f42958d16ec66681bd0c3cc7b210d
Simon Rozsival <simon@rozsival.com>
2018-10-22 07:55:35 +0000

    :white_check_mark: Check devices status before creating a container

--- racer-raspberry/build-and-run.sh
+++ racer-raspberry/build-and-run.sh
@@ -11,10 +11,22 @@ IMAGE_NAME=$1
 CONTAINER_NAME=$2
 
 # test if the Scanse Sweep Lidar is connected to the correct port
-# ???
+SPEEP_PORT="/dev/ttyUSB0"
+ls -l $SWEEP_PORT > /dev/null 2> /dev/null
+if [ $? -ne 0 ]
+then
+	echo "The Scanse Sweep Lidar is not connected on $SWEEP_PORT"
+	exit 1
+fi
 
-# test if the Razor IMU 9DoF is connected to the correct port
-# ???
+# test if the Bosch IMU is connected to the correct port
+IMU_PORT="/dev/ttyACM0"
+ls -l $IMU_PORT > /dev/null 2> /dev/null
+if [ $? -ne 0 ]
+then
+	echo "Thie Bosch IMU BNO055 is not connected on $IMU_PORT"
+	exit 2
+fi
 
 # build docker image
 echo "> build image $IMAGE_NAME"
@@ -24,7 +36,8 @@ docker build -t $IMAGE_NAME ./src
 echo
 echo "> run the image $IMAGE_NAME as container $CONTAINER_NAME"
 docker run \
-        --device /dev/imu \
+        --device $IMU_PORT:/dev/imu \
+        --device $SWEEP_PORT:/dev/sweep \
 	--net=host \
 	--publish-all \
 	--name $CONTAINER_NAME \
