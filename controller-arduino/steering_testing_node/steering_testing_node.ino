327bea5d789dcb9cf98f1e2663f71a5f70f08d51
Simon Rozsival <simon@rozsival.com>
2019-07-29 20:23:49 +0200

    :race_car: Testing logic for the controller

---
+++ controller-arduino/steering_testing_node/steering_testing_node.ino
@@ -0,0 +1,64 @@
+
+/*
+   Raceing car steering
+   - receives commands from the '/racer/commands' topic from ROS through serial port
+   - translates steering commands into servo control
+   - the throttle is taken just from the RC controller
+
+   Please make sure you have installed the `ros_lib` library for running ROS node on an Arduino with the
+   `ros-kinetic-angles` package installed (so the `geometry_msgs/Twist.h` header file is available).
+*/
+
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+#include <Servo.h>
+
+#include "./utils.h"
+#include "./driving_logic.h"
+
+#define PIN_OUTPUT_THROTTLE 10
+#define PIN_OUTPUT_STEERING 9
+
+#define DRIVING_TOPIC "/racer/commands"
+
+ros::NodeHandle node_handle;
+
+ros::Subscriber<geometry_msgs::Twist> driving(DRIVING_TOPIC, &drive_callback);
+
+Servo servo_steering;
+Servo servo_throttle;
+
+void setup() {
+  node_handle.initNode();
+  node_handle.subscribe(driving);
+
+  attach_rc_input_interrupts();
+  init_driving_logic();
+
+  servo_steering.attach(PIN_OUTPUT_STEERING);
+  servo_throttle.attach(PIN_OUTPUT_THROTTLE);
+}
+
+void loop() {
+  node_handle.spinOnce();
+
+  int steering_pwm, throttle_pwm;
+  select_inputs(&steering_pwm, &throttle_pwm);
+
+  servo_steering.writeMicroseconds(steering_pwm);
+
+  if (!reverse && throttle_pwm < THROTTLE_NONE_PWM - PWM_OFF_CENTER_TOLERANCE) {
+    // switching into reverse is a bit more complicated than just setting PWM between THROTTLE_REVERSE_PWM and THROTTLE_NONE_PWM
+    // the PWM has to be set to THROTTLE_REVERSE_PWM for a while, then return back to THROTTLE_NONE_PWM and then go to the `autonomous_throttle` value
+    // this will dealy the algorithm a bit and it will 
+    servo_throttle.writeMicroseconds(THROTTLE_REVERSE_PWM);
+    delay(1 * DUTY_CYCLE_MS);
+    servo_throttle.writeMicroseconds(THROTTLE_NONE_PWM);
+    delay(3 * DUTY_CYCLE_MS); // 3 was the minimum constant which worked for me
+  }
+
+  servo_throttle.writeMicroseconds(throttle_pwm);
+  reverse = throttle_pwm < THROTTLE_NONE_PWM - PWM_OFF_CENTER_TOLERANCE;
+
+  delay(DUTY_CYCLE_MS);
+}
