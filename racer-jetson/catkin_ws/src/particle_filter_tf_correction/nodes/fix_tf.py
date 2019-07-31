bb70c6b50814499a9832f35f7cffc97877e1638c
Simon Rozsival <simon@rozsival.com>
2019-07-31 13:48:57 +0200

    :bug: Fix linear transformations

--- racer-jetson/catkin_ws/src/particle_filter_tf_correction/nodes/fix_tf.py
+++ racer-jetson/catkin_ws/src/particle_filter_tf_correction/nodes/fix_tf.py
@@ -2,24 +2,31 @@
 
 import rospy
 import tf
+import numpy as np
+
+from tf import transformations as t
 from nav_msgs.msg import Odometry
 
 def pose_update(msg, args):
   listener, map_frame, odom_frame, base_link_frame = args
   br = tf.TransformBroadcaster()
 
-  base_link_position, base_link_rotation_q = listener.lookupTransform(odom_frame, base_link_frame, rospy.Time(0))
-  base_link_euler = tf.transformations.euler_from_quaternion(base_link_rotation_q)
+  # odom -> base_link transformation
+  trans, rot = listener.lookupTransform(odom_frame, base_link_frame, rospy.Time(0))
+  odom_to_base = t.concatenate_matrices(t.translation_matrix(trans), t.quaternion_matrix(rot))
 
+  # map -> base_link transformation (from the msg)
+  p = msg.pose.pose.position
   o = msg.pose.pose.orientation
-  quat = (o.x, o.y, o.z, o.w)
-  absolute_euler = tf.transformations.euler_from_quaternion(quat)
+  map_to_base = t.concatenate_matrices(t.translation_matrix((p.x, p.y, p.z)), t.quaternion_matrix((o.x, o.y, o.z, o.w)))
 
-  dx = msg.pose.pose.position.x - base_link_position[0]
-  dy = msg.pose.pose.position.y - base_link_position[1]
-  dtheta = base_link_euler[2] - base_link_euler[2]
+  # map -> odom = (map -> base_link) * (base_link -> odom) = (map -> base_link) * (odom -> base_link)^-1
+  base_to_odom = t.inverse_matrix(odom_to_base)
+  map_to_odom = np.matmul(map_to_base, base_to_odom)
+  map_to_odom_trans = t.translation_from_matrix(map_to_odom)
+  map_to_odom_rot = t.quaternion_from_matrix(map_to_odom)
 
-  br.sendTransform((dx, dy, 0), tf.transformations.quaternion_from_euler(0, 0, dtheta), rospy.Time.now(), odom_frame, map_frame)
+  br.sendTransform(map_to_odom_trans, map_to_odom_rot, rospy.Time.now(), odom_frame, map_frame)
 
 if __name__ == '__main__':
   rospy.init_node('fix_tf')
