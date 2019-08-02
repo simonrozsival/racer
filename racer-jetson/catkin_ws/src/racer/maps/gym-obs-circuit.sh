93c471180b3b968439beca54a0e41d3839b109c3
Simon Rozsival <simon@rozsival.com>
2019-08-02 18:29:42 +0200

    :bug: Fix segfault

--- racer-jetson/catkin_ws/src/racer/maps/gym-obs-circuit.sh
+++ racer-jetson/catkin_ws/src/racer/maps/gym-obs-circuit.sh
@@ -1,2 +1,2 @@
-rostopic pub --once /racer/circuit racer/CircuitMsg '{check_points: [[6.382, -2.770, 0.000],[-1.586, -2.134, 0.000]]}'
+rostopic pub --once /racer/circuit racer_msgs/Circuit '{check_points: [[6.382, -2.770, 0.000],[-1.586, -2.134, 0.000]]}'
 
