9ef5110faf58a84dd87650a1dbd395f87a241330
Simon Rozsival <simon@rozsival.com>
2019-08-03 11:38:38 +0200

    :bug: Update pure pursuit algorithm

--- racer-jetson/catkin_ws/src/racer/include/racing/following_strategies/pure_pursuit.h
+++ racer-jetson/catkin_ws/src/racer/include/racing/following_strategies/pure_pursuit.h
@@ -4,6 +4,8 @@
 #include "racing/vehicle_model/vehicle.h"
 #include "racing/vehicle_model/kinematic_bicycle_model.h"
 
+#include "racing/math/primitives.h"
+
 namespace racing {
 
   class pure_pursuit {
@@ -17,26 +19,32 @@ namespace racing {
     }
 
     double select_steering_angle(const kinematic_model::state& current_state, int passed_waypoints, const kinematic_model::trajectory& trajectory) const {
-      const auto reference = find_reference_position(current_state, passed_waypoints, trajectory);
+      const double lookahead = std::min(min_lookahead_, current_state.speed * lookahead_coef_);
+      const auto reference = find_reference_position(
+        current_state, passed_waypoints, trajectory, lookahead);
 
-      auto diff = reference.location() - current_state.position.location();
+      auto diff = reference.location() - calculate_rear_axle_center(center);
       double alpha = atan2(diff.y, diff.x) - current_state.position.heading_angle;
 
       return atan(2 * vehicle_.wheelbase * sin(alpha) / lookahead);
     }
 
-    vehicle_position find_reference_position(const kinematic_model::state& current_state, int passed_waypoints, const kinematic_model::trajectory& trajectory) const {
+    vehicle_position find_reference_position(
+      const kinematic_model::state& current_state,
+      int passed_waypoints,
+      const kinematic_model::trajectory& trajectory,
+      const double lookahead) const {
       auto sub_trajectory = trajectory.find_reference_subtrajectory(current_state, passed_waypoints);
-
+      
       if (!sub_trajectory || sub_trajectory->steps.size() == 0) {
         return 0.0;
       }
 
-      const double lookahead = std::min(min_lookahead_, current_state.speed * lookahead_coef_);
-      const double lookahead_sq = lookahead * lookahead;
+      auto rear_axle_center = calculate_rear_axle_center(current_state);
 
+      const double lookahead_sq = lookahead * lookahead;
       auto reference_step = sub_trajectory->steps.begin();
-      while (reference_step != sub_trajectory->steps.end() && (reference_step->step.position.location() - current_state.position.location()).length_sq() < lookahead_sq) {
+      while (reference_step != sub_trajectory->steps.end() && (reference_step->step.position.distance_sq(rear_axle_center) < lookahead_sq) {
         ++reference_step;
       }
 
@@ -49,6 +57,11 @@ namespace racing {
     const vehicle& vehicle_;
     double min_lookahead_;
     double lookahead_coef_;
+
+    math::point calculate_rear_axle_center(const kinematic_model::state& current_state) const {
+      auto rear_wheel_offset = math::vector(-vehicle_.wheelbase / 2).rotate(current_state.position.heading_angle);
+      return current_state.position.location() + rear_wheel_offset;
+    }
   };
 
 }
