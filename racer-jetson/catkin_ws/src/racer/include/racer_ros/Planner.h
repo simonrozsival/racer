981e1db76a68a0700322ec81e90da8d0be952f92
Simon Rozsival <simon@rozsival.com>
2020-04-04 15:49:00 +0200

    :construction: Continuing with cleanup

--- racer-jetson/catkin_ws/src/racer/include/racer_ros/Planner.h
+++ racer-jetson/catkin_ws/src/racer/include/racer_ros/Planner.h
@@ -2,58 +2,44 @@
 
 #include <vector>
 
+#include "geometry_msgs/PoseStamped.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include "nav_msgs/Odometry.h"
-#include "geometry_msgs/PoseStamped.h"
 #include "racer_msgs/Trajectory.h"
 #include "racer_msgs/Waypoints.h"
 
 #include "racer_ros/utils.h"
 
-#include "racer/math.h"
 #include "racer/action.h"
-#include "racer/trajectory.h"
-#include "racer/vehicle_model/kinematic_model.h"
-#include "racer/vehicle_configuration.h"
+#include "racer/math.h"
 #include "racer/track/collision_detection.h"
+#include "racer/trajectory.h"
+#include "racer/vehicle_configuration.h"
+#include "racer/vehicle_model/kinematic_model.h"
 
 #include "racer/astar/discretized_search_problem.h"
 #include "racer/astar/sehs.h"
 
 namespace racer_ros
 {
-
 template <typename State, typename DiscreteState>
 class Planner
 {
 public:
-  Planner(
-      std::shared_ptr<racer::vehicle_model::vehicle_model<State>> model,
-      std::unique_ptr<racer::astar::discretization<DiscreteState, State>> discretization,
-      const double time_step_s,
-      const std::string map_frame_id)
-      : model_(model),
-        discretization_(std::move(discretization)),
-        time_step_s_(time_step_s),
-        map_frame_(map_frame_id)
+  Planner(std::shared_ptr<racer::vehicle_model::vehicle_model<State>> model,
+          std::unique_ptr<racer::astar::discretization<DiscreteState, State>> discretization, const double time_step_s,
+          const std::string map_frame_id)
+    : model_(model), discretization_(std::move(discretization)), time_step_s_(time_step_s), map_frame_(map_frame_id)
   {
   }
 
-  std::unique_ptr<racer_msgs::Trajectory> plan(
-      const State &initial_state,
-      const std::vector<racer::action> &available_actions,
-      const std::shared_ptr<racer::circuit> circuit,
-      const std::shared_ptr<racer::track::collision_detection> collision_detector,
-      const int next_waypoint) const
+  std::unique_ptr<racer_msgs::Trajectory>
+  plan(const State &initial_state, const std::vector<racer::action> &available_actions,
+       const std::shared_ptr<racer::circuit> circuit,
+       const std::shared_ptr<racer::track::collision_detection> collision_detector, const int next_waypoint) const
   {
     auto problem = std::make_unique<racer::astar::discretized_search_problem<DiscreteState, State>>(
-        initial_state,
-        time_step_s_,
-        available_actions,
-        discretization_,
-        model_,
-        circuit,
-        collision_detector);
+        initial_state, time_step_s_, available_actions, discretization_, model_, circuit, collision_detector);
 
     std::atomic<bool> terminate = false;
     const auto result = racer::astar::search<DiscreteState, State>(std::move(problem), terminate);
@@ -104,4 +90,4 @@ private:
   const std::string map_frame_;
 };
 
-} // namespace racer_ros
+}  // namespace racer_ros
