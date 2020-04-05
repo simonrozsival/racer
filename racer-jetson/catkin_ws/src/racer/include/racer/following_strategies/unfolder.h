65b98b131a49f8eba6fe3c6069adae05545c27e0
Simon Rozsival <simon@rozsival.com>
2020-04-05 18:55:59 +0200

    :construction: That's all for today

--- racer-jetson/catkin_ws/src/racer/include/racer/following_strategies/unfolder.h
+++ racer-jetson/catkin_ws/src/racer/include/racer/following_strategies/unfolder.h
@@ -40,6 +40,22 @@ public:
     return next_states;
   }
 
+  auto unfold_unsafe(const State &origin, const racer::action &action) const {
+    std::vector<State> next_states{};
+
+    next_states.push_back(origin);
+    State last_state = origin;
+
+    for (int i = 0; i < steps_; ++i) {
+      // the obstacles in the map are inflated so it is sufficient to check
+      // just the grid cell which the center of the vehicle lies in
+      last_state = model_->predict_next_state(last_state, action, time_step_s_);
+      next_states.push_back(last_state);
+    }
+
+    return next_states;
+  }
+
 private:
   std::shared_ptr<racer::vehicle_model::vehicle_model<State>> model_;
   double time_step_s_;
