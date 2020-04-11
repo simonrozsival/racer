7edd213fb9914bf10a7f1ea5c3b671d1edaaf95f
Simon Rozsival <simon@rozsival.com>
2020-04-11 21:31:00 +0200

    :see_no_evil: Fix incorrect sign bug

--- racer-jetson/catkin_ws/src/racer/include/racer/vehicle_model/steering_servo_model.h
+++ racer-jetson/catkin_ws/src/racer/include/racer/vehicle_model/steering_servo_model.h
@@ -23,22 +23,24 @@ private:
 public:
   steering_servo_model(racer::math::angle max_steering_angle_left, racer::math::angle max_steering_angle_right,
                        std::array<double, 2> angle_to_pwm, std::array<double, 2> pwm_adjustment_coefficients)
-      : max_steering_angle_left_{max_steering_angle_left}, max_steering_angle_right_{max_steering_angle_right}, angle_to_pwm_{angle_to_pwm}, pwm_adjustment_coefficients_{pwm_adjustment_coefficients}
+    : max_steering_angle_left_{ max_steering_angle_left }
+    , max_steering_angle_right_{ max_steering_angle_right }
+    , angle_to_pwm_{ angle_to_pwm }
+    , pwm_adjustment_coefficients_{ pwm_adjustment_coefficients }
   {
   }
 
   static auto rc_beast()
   {
-    return std::make_unique<steering_servo_model>(angle::from_degrees(-21.16), angle::from_degrees(26.57),
-                                                  std::array<double, 2>{12.701, 1476.686},
-                                                  std::array<double, 2>{0.000329, 0.1174});
+    return std::make_unique<steering_servo_model>(angle::from_degrees(21.16), angle::from_degrees(-26.57),
+                                                  std::array<double, 2>{ 12.701, 1476.686 },
+                                                  std::array<double, 2>{ 0.000329, 0.1174 });
   }
 
   static auto simulator()
   {
-    return std::make_unique<steering_servo_model>(angle::from_degrees(-20), angle::from_degrees(20),
-                                                  std::array<double, 2>{12.701, 1476.686},
-                                                  std::array<double, 2>{0.000329, 0.1174});
+    return std::make_unique<steering_servo_model>(1.0, -1.0, std::array<double, 2>{ 12.701, 1476.686 },
+                                                  std::array<double, 2>{ 0.000329, 0.1174 });
   }
 
   angle predict_next_state(const angle &current_steering_angle, const racer::action &action, const double dt) const
@@ -61,7 +63,7 @@ public:
   double action_input_for_angle(const angle &target) const
   {
     const angle max = target > 0 ? max_steering_angle_left_ : max_steering_angle_right_;
-    return -std::abs(target) / max;
+    return std::abs(target) / max;
   }
 
   angle max_steering_angle() const
@@ -82,4 +84,4 @@ private:
   }
 };
 
-} // namespace racer::vehicle_model
+}  // namespace racer::vehicle_model
