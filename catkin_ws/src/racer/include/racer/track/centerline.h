643f125aff86a8d74e729688f60f93a21cf3df63
Šimon Rozsíval <simon@rozsival.com>
2020-05-13 20:53:12 +0200

    :memo: More cpp docs

--- catkin_ws/src/racer/include/racer/track/centerline.h
+++ catkin_ws/src/racer/include/racer/track/centerline.h
@@ -12,45 +12,45 @@
 namespace racer::track
 {
 
-class centerline
-{
-private:
-  centerline(std::vector<racer::math::point> points, double width)
-      : points_{points}, width_{width}
+  class centerline
   {
-  }
-
-public:
-  static centerline find(const racer::vehicle::configuration &start,
-                          const std::shared_ptr<racer::track::occupancy_grid> grid,
-                          const std::vector<racer::math::point> &checkpoints)
-  {
-    const auto adjusted_start = grid->move_towards_center(start);
-    const auto radius =
-        grid->distance_to_closest_obstacle(adjusted_start.location(), 5.0);
-
-    racer::sehs::space_exploration space_exploration{0.6 * radius, 2.0 * radius,
-                                                      12};
-    const auto circles =
-        space_exploration.explore_grid(grid, adjusted_start, checkpoints);
-
-    std::vector<racer::math::point> points;
-    points.reserve(circles.size());
-    for (const auto &c : circles)
+  private:
+    centerline(std::vector<racer::math::point> points, double width)
+        : points_{points}, width_{width}
     {
-      points.push_back(c.center());
     }
 
-    return centerline{points, 2 * radius};
-  }
+  public:
+    static centerline find(const racer::vehicle::configuration &start,
+                           const std::shared_ptr<racer::track::occupancy_grid> grid,
+                           const std::vector<racer::math::point> &checkpoints)
+    {
+      const auto adjusted_start = grid->move_towards_center(start);
+      const auto radius =
+          grid->distance_to_closest_obstacle(adjusted_start.location(), 5.0);
 
-  bool empty() const { return points_.empty(); }
-  double width() const { return width_; }
-  const std::vector<racer::math::point> points() const { return points_; }
+      racer::sehs::space_exploration space_exploration{0.6 * radius, 2.0 * radius,
+                                                       12};
+      const auto circles =
+          space_exploration.explore_grid(grid, adjusted_start, checkpoints);
 
-private:
-  const std::vector<racer::math::point> points_;
-  const double width_;
-};
+      std::vector<racer::math::point> points;
+      points.reserve(circles.size());
+      for (const auto &c : circles)
+      {
+        points.push_back(c.center());
+      }
+
+      return centerline{points, 2 * radius};
+    }
+
+    bool empty() const { return points_.empty(); }
+    double width() const { return width_; }
+    const std::vector<racer::math::point> points() const { return points_; }
+
+  private:
+    const std::vector<racer::math::point> points_;
+    const double width_;
+  };
 
 } // namespace racer::track
